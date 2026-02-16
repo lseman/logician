ad8a0aef3d5047fc95d840800266d4ab22ae7cf1
Laio O. Seman <laioseman@gmail.com>
2026-02-16 12:02:08 -0300

    adjust gitignore

--- plot_helpers.py
+++
@@ -1,9 +0,0 @@
-"""
-Backward-compatible wrapper.
-
-Use `apps.plotting.embedding_map` for the canonical module location.
-"""
-
-from apps.plotting.embedding_map import plot_session_embedding_map
-
-__all__ = ["plot_session_embedding_map"]
