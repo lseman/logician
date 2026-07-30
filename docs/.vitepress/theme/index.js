8d0175ca557b9ba8a117f28d640c503669eddd8f
Laio O. Seman <laioseman@gmail.com>
2026-07-30 10:16:52 -0300

    style: align docs theme to landing page palette (cyan→violet, navy-dark bg)

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import { initMermaid } from './mermaid-init'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  setup() {
+    DefaultTheme.setup?.()
+    initMermaid()
+  },
+  onContentUpdated() {
+    initMermaid()
+  },
+}
